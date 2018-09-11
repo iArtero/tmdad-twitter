20ced309635325fd9147d84cf33aa838341855c3
Ismael <pedroismaelartero@gmail.com>
2018-09-11 19:47:03 +0200

    fix errors?

--- tweetMiddle/src/main/resources/static/customJs/loadPage.js
+++ tweetMiddle/src/main/resources/static/customJs/loadPage.js
@@ -19,8 +19,8 @@ $
                 if (!(/^http:.*/.test(settings.url) || /^https:.*/
                         .test(settings.url))) {
                     // Only send the token to relative URLs i.e. locally.
-                    xhr.setRequestHeader("X-XSRF-TOKEN",
-                        Cookies.get('XSRF-TOKEN'));
+                    //xhr.setRequestHeader("X-XSRF-TOKEN",
+                    //    Cookies.get('XSRF-TOKEN'));
                 }
             }
         }
